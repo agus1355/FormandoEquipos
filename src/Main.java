7b0e86efc8a8a4f6e76df70c56b533de099791fc
Agustin <ratto_agustin@hotmailcom>
2021-05-24 07:27:50 -0300

    agregados casos de prueba finales

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Archivo a = new Archivo();
 		
-		List<String> datos = a.leer("in\\caso_01_no_hay_equipo.in");
+		List<String> datos = a.leer("in\\caso_06_empate_entre_equipos.in");
 		
 		Equipo equipo = new Equipo(datos);
 		
